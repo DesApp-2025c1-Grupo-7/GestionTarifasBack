cae59fbb9dde2bb7ce2764d0cf56e07dad3e1ed8
Nicolas Arcamone <109641395+nicolasArcamone97@users.noreply.github.com>
2025-05-13 23:22:43 -0300

    feat: agregado de consulta por id + create

--- src/tipo-carga/controllers/tipo-carga.controller.ts
+++ src/tipo-carga/controllers/tipo-carga.controller.ts
@@ -1,23 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 import { TipoCargaService } from '../services/tipo-carga.service';
 import { TipoCarga } from '../entities/tipo-carga.entity';
+import { TipoCargaDTO } from '../dtos/tipo-carga.dto';
 
 @Controller('tipo-carga')
 export class TipoCargaController {
     
     constructor( private readonly cargaService:TipoCargaService){}
 
-    
+    @HttpCode(HttpStatus.OK)
     @Get('all')
     obtenerCargas():Promise<TipoCarga[]> {
         return this.cargaService.obtenerCargas()
     }
 
-    
+    @HttpCode(HttpStatus.OK)
+    @Get(':id')
+    obtenerCarga(@Param('id') id:number): Promise<TipoCarga> {
+        return this.cargaService.obtenerCarga(id)
+    }
 
 
-
-
+    @HttpCode(HttpStatus.CREATED) 
+    @Post()
+    crearCarga(@Body() body:TipoCargaDTO){
+        return this.cargaService.crearTipoCarga(body)
+    }
 
 
 }
--- src/tipo-carga/services/tipo-carga.service.ts
+++ src/tipo-carga/services/tipo-carga.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TipoCarga } from '../entities/tipo-carga.entity';
 import { Repository } from 'typeorm';
+import { TipoCargaDTO } from '../dtos/tipo-carga.dto';
 
 @Injectable()
 export class TipoCargaService {
 
     constructor(@InjectRepository(TipoCarga) private readonly cargaRepository:Repository<TipoCarga>){}
+
+    private readonly logger = new Logger(TipoCargaService.name)
 
     
     public async obtenerCargas(): Promise<TipoCarga[]> {
@@ -16,8 +19,46 @@ export class TipoCargaService {
         return cargas
     }
 
+    public async obtenerCarga(cargaId:number): Promise<TipoCarga>{
+        try{
+            const cargaExistente = await this.cargaRepository.findOne({where: {id:cargaId}})
 
-    
+            if(!cargaExistente){
+                throw new BadRequestException("El tipo de carga con el id buscado no exisite")
+            }
+
+            return cargaExistente
+        } catch (error) {
+            this.logger.error("Error al obtener el tipo de carga", error.stack)
+            throw new InternalServerErrorException('Ocurrió un error al buscar el tipo de carga. Intente nuevamente.');
+        }
+    }
+
+
+    public async crearTipoCarga(body: TipoCargaDTO): Promise<TipoCarga> {
+        try {
+            // Validar si ya existe un tipo de carga idéntico para no generar duplicado
+            const cargaExistente = await this.cargaRepository.findOne({
+                where: {
+                    categoria: body.categoria,
+                    requisitoEspecial: body.requisitoEspecial,
+                    pesoTotal: body.pesoTotal,
+                    volumenTotal: body.volumenTotal,
+                    valorBase: body.valorBase,
+                },
+            });
+
+            if (cargaExistente) {
+                throw new ConflictException('Ya existe un tipo de carga con esos mismos valores.');
+            }
+
+            const nuevaCarga = this.cargaRepository.create(body);
+            return await this.cargaRepository.save(nuevaCarga);
+        } catch (error) {
+            this.logger.error('Error al guardar el tipo de carga', error.stack);
+            throw new InternalServerErrorException('Ocurrió un error al guardar el tipo de carga. Intente nuevamente.');
+        }
+    }
 
     
 }
