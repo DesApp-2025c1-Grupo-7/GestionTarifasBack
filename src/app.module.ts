55c5ebb8facc6b6867c436965dcd780af82c97c9
Nicolas Arcamone <109641395+nicolasArcamone97@users.noreply.github.com>
2025-05-13 22:08:49 -0300

    integracion ORM + config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TipoCargaModule } from './tipo-carga/tipo-carga.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { dbConfig } from './config/db.config';
 
 @Module({
-  imports: [TipoCargaModule],
+  imports: [
+    TypeOrmModule.forRoot(dbConfig),
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TipoCargaModule ],
   controllers: [],
   providers: [],
 })
