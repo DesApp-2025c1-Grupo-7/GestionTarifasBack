eedf905b11ee79d5a79898240b72327c6604bfb9
Nicolas Arcamone <109641395+nicolasArcamone97@users.noreply.github.com>
2025-06-10 22:19:38 -0300

    implementacion @ApiProperty
    agregado de ApiProperty para mejorar documentación Swagger

--- src/transportista/dtos/transportista.dto.ts
+++ src/transportista/dtos/transportista.dto.ts
@@ -1,22 +1,43 @@
-import { IsString, IsNotEmpty, IsNumber, IsArray} from 'class-validator';
+import { IsString, IsNotEmpty, IsNumber, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTransportistaDto {
+  @ApiProperty({
+    example: 'Transporte Martínez S.A.',
+    description: 'Nombre del transportista',
+  })
   @IsString()
   @IsNotEmpty()
   nombre: string;
 
+  @ApiProperty({
+    example: 5000,
+    description: 'Costo fijo del servicio de transporte',
+  })
   @IsNumber()
   costoServicio: number;
 
+  @ApiProperty({
+    example: 'martinez@transportes.com',
+    description: 'Información de contacto del transportista',
+  })
   @IsString()
   @IsNotEmpty()
   contacto: string;
 
+  @ApiProperty({
+    example: '+54 9 11 2345-6789',
+    description: 'Número de teléfono del transportista',
+  })
   @IsString()
   @IsNotEmpty()
   telefono: string;
 
+  @ApiProperty({
+  example: [1, 2, 3],
+  description: 'IDs de vehículos existentes a asociar con el nuevo transportista',
+  })
   @IsArray()
-  @IsNumber({}, { each: true }) // valida que cada item sea un número
+  @IsNumber({}, { each: true })
   vehiculos: number[];
 }
